dbeec85f208efe3f59e0dc2f147450f0b8475613
SHW378 <cesarbrmn@hotmail.com>
2024-11-15 15:51:11 -0600

    modularización

--- modularización/src/App.java
+++ modularización/src/App.java
@@ -1,5 +1,10 @@
 public class App {
+public static int enSegundos(int horas , int minutos , int segundos){
+    int segsal; 
+    segsal = horas*3600 + minutos*60 + segundos;
+    return segsal;
+}
+
     public static void main(String[] args) {
-        
     }
 }
