41e93bb7cde568c52172890cae62c412e5c9e54c
SHW378 <cesarbrmn@hotmail.com>
2024-11-20 15:01:05 -0600

    .

--- modularización/src/App.java
+++ modularización/src/App.java
@@ -1,10 +1,24 @@
+import java.util.Scanner;
 public class App {
 public static int enSegundos(int horas , int minutos , int segundos){
     int segsal; 
     segsal = horas*3600 + minutos*60 + segundos;
     return segsal;
 }
-
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int hr, min, seg, segFin, segInicio;
+        System.out.println("Ingrese hora, minuto y segundo de la hora de inicio");
+        hr = sc.nextInt();
+        min = sc.nextInt();
+        seg = sc.nextInt();
+        segInicio = enSegundos(hr, min, seg);
+        System.out.println("Ingrese hora, minuto y segundo de la hora de finalizacion");
+        hr = sc.nextInt();
+        min = sc.nextInt();
+        seg = sc.nextInt();
+        segFin = enSegundos(hr, min, seg);
+        System.out.println("La duracion es de " + segFin + " segundos");
+        sc.close();
     }
 }
