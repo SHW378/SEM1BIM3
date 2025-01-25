cff647c332d4e5e66479e5660353e70b5b73357e
SHW378 <cesarbrmn@hotmail.com>
2025-01-25 14:53:23 -0600

    12

--- modularización/src/App.java
+++ modularización/src/App.java
@@ -7,12 +7,11 @@ public static int enSegundos(int horas , int minutos , int segundos){
 }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int hr, min, seg, segFin, segInicio;
+        int hr, min, seg, segFin;
         System.out.println("Ingrese hora, minuto y segundo de la hora de inicio");
         hr = sc.nextInt();
         min = sc.nextInt();
         seg = sc.nextInt();
-        segInicio = enSegundos(hr, min, seg);
         System.out.println("Ingrese hora, minuto y segundo de la hora de finalizacion");
         hr = sc.nextInt();
         min = sc.nextInt();
