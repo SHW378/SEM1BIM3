a021a2750bdd1e8677228193d6ba9d25274acb79
SHW378 <cesarbrmn@hotmail.com>
2025-05-01 22:53:54 -0600

    2

--- modularización/src/App.java
+++ modularización/src/App.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 public class App {
-public static int enSegundos(int horas , int minutos , int segundos){
-    int segsal; 
-    segsal = horas*3600 + minutos*60 + segundos;
-    return segsal;
-}
+    public static int enSegundos(int horas , int minutos , int segundos){
+        int segsal; 
+        segsal = horas*3600 + minutos*60 + segundos;
+        return segsal;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int hr, min, seg, segFin;
